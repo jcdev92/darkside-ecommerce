fbd80af9d631c46e805a2a98dbdd1a467e054c00
Jesus Clemente <jcdeveloper92@gmail.com>
2022-09-01 21:20:06 -0400

    xyz

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <Header>
-      <NavLink to='/'>
+      <NavLink to="/">
         <h1 className="header__logo"></h1>
       </NavLink>
       <nav className="header__nav">
         <ul className="header__list">
-          <li className="header__item"><NavLink to='login'/></li>
-          <li className="header__item"><NavLink to='/purchases'/></li>
+          <li className="header__item">
+            <NavLink className="header__link" to="login" />
+          </li>
+          <li className="header__item">
+            <NavLink className="header__link" to="/purchases" />
+          </li>
+          <li className="header__item">
+            <h2 className="header__link">Cart</h2>
+          </li>
         </ul>
       </nav>
     </Header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
