11531b9a1fb70bba095a1724134ebcc2fce0db99
Jesus Clemente <jcdeveloper92@gmail.com>
2022-09-05 21:23:57 -0400

    xyz

--- src/components/Routes/Login.jsx
+++ src/components/Routes/Login.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import FormLogin from '../Login/FormLogin'
 
 const Login = () => {
   return (
     <section>
        <div className="empty__box"></div>
       <h1>Login</h1>
+      <FormLogin />
     </section>
   )
 }
